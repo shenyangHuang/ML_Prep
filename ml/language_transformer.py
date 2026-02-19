9c16e0124031dfbea240c8d2f20fce91894984ea
Andy Huang <shenyang.huang@mail.mcgill.ca>
2026-02-19 18:37:05 -0500

    adding scripts

--- ml/language_transformer.py
+++ ml/language_transformer.py
@@ -52,6 +52,8 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
         super.__init__()
         pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+
 
 
 
